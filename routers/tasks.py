8e1f63a01f506e4dff614fc818ca2b9360c3035e
Bahar <fatemehkhosravi.002@gmail.com>
2026-05-13 15:54:59 +0330

    added PUT endpoint

--- routers/tasks.py
+++ routers/tasks.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from database import get_db
 from app.database import SessionLocal
 from app import crud, schemas
 
@@ -44,4 +44,19 @@ def delete_task(task_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Task not found")
 
     return {"message": "Task deleted successfully"}
+
+
+@router.put("/tasks/{task_id}", response_model=schemas.TaskResponse)
+def update_task(
+    task_id: int,
+    updated_task: schemas.TaskUpdate,
+    db: Session = Depends(get_db)
+):
+    task = crud.update_task(db, task_id, updated_task)
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return task
+
 

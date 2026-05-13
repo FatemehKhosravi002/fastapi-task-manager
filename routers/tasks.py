6641dce87bff406a94b88d498683b041085e5238
Bahar <fatemehkhosravi.002@gmail.com>
2026-05-13 14:46:59 +0330

    add task routers and api endpoints

---
+++ routers/tasks.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database import SessionLocal
+from app import crud, schemas
+
+
+router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/tasks", response_model=schemas.TaskResponse)
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    return crud.create_task(db=db, task=task)
+
+
+@router.get("/tasks", response_model=list[schemas.TaskResponse])
+def read_tasks(db: Session = Depends(get_db)):
+    return crud.get_tasks(db)
+
+
+@router.get("/tasks/{task_id}", response_model=schemas.TaskResponse)
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    task = crud.get_task(db, task_id)
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return task
+
+
+@router.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = crud.delete_task(db, task_id)
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return {"message": "Task deleted successfully"}
+
